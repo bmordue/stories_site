510f411314734193df76f718162424b22005c0fd
Ben Mordue <bmordue@adobe.com>
2016-05-06 12:10:49 +0100

    Restructure
    - Use bower for dependency management
    - Use Bootstrap framework
    - Reorganize project files

---
+++ assets/js/stories.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+  $.getJSON( "http://crissic.benmordue.co.uk:8000/api/stories?_limit=20&_order[comments]=desc", function( data ) {
+    var html_rows = [];
+    var json_rows = data.json;
+
+    $.each( json_rows, function( index, val ) {
+      html_rows.push("<tr><td>" + index + "</td><td>" + val.hnurl + " (<a href='" + val.url + "'>article</a>)</td><td>" + val.added + "</td><td><a href='" + val.hnurl + "'>" + val.comments + "</a></td><td>" + val.starred + "</td></tr>\n");
+    });
+    $("#results_table").html(html_rows);      
+  })
+  .fail(function() {
+    console.log('Failed to fetch results');
+  })
+});
